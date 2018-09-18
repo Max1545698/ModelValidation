39ecbe03df9f5d484c04bec68d696f34b1a33e33
Максим Рожок <max15071999@gmail.com>
2018-09-18 16:58:45 +0300

    Add project files.

---
+++ ModelValidation/wwwroot/js/modelValidationStyle.js
@@ -0,0 +1,13 @@
+﻿var inputs = document.getElementsByTagName("input");
+
+for (let i = 0; i < inputs.length; i++) {
+    // if (inputs[i].classList.contains("input-validation-error")) {
+    inputs[i].classList.replace("input-validation-error", "border-danger")
+    //if (inputs[i].type === "checkbox") {
+    //    inputs[i].parentElement.innerHTML += "<div id=\"errMsg\">You should agreed with the statment</div>";
+    //    document.getElementById("errMsg").classList.add("text-danger");
+    //}
+}
+//}
+
+
